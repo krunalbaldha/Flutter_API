acf99e73f7c25256b6e9a7065255f7d027622f8b
krunalbaldha <krunalbaldha1@gmail.com>
2023-05-06 08:29:16 +0530

    API First Page

---
+++ lib/model/article.dart
@@ -0,0 +1,25 @@
+class Article{
+  final String author;
+  final String title;
+  final String? description;
+  final String urltoImage;
+  final String publishedAt;
+
+  Article({
+    required this.author,
+    required this.title,
+    this.description,
+    this.urltoImage,
+    required this.publishedAt
+  });
+
+  factory Article.fromJson(Map<String,dynamic> json){
+    return Article(
+        author: json['author']==null ? null :json['author'],
+        title: json['title']==null ? null :json['title'],
+        description: json['description']==null ? null :json['description'],
+        urltoImage: json['urltoImage']==null ? null :json['urltoImage'],
+        publishedAt: json['publishedAt']==null ? null :json['publishedAt']
+    );
+  }
+}
